9f19855a02656177af851cb952b2861893326628
Vishal <vishalbansal.vdoit@gmail.com>
2025-02-20 10:11:04 +0530

    changes in userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import{ deleteProfileController, getAllProfilesController, getUserProfileController, loginController, logoutController, registerController, updateProfileController } from '../controllers/userController.js'
+import{getAllProfilesController, getUserProfileController, loginController, logoutController, registerController, updateProfileController } from '../controllers/userController.js'
 import { isAdmin, isAuth } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
@@ -10,7 +10,6 @@ router.get("/profile", isAuth, getUserProfileController);
 router.get("/logout", isAuth, logoutController);
 router.put("/profile-update", isAuth, updateProfileController);
 router.get("/get-allprofiles", isAuth, isAdmin, getAllProfilesController);
-router.delete("/delete-profile/:id", isAuth, isAdmin, deleteProfileController);
 export default router
 
 
